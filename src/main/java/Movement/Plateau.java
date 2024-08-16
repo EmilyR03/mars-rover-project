b885edb2fc2151777c73f179a4c36d2eb8cc75e7
Emily Rhodes <erhodes.345@gmail.com>
2024-08-16 10:43:28 +0100

    feat: add setters and getters to Position Class

---
+++ src/main/java/Movement/Plateau.java
@@ -0,0 +1,15 @@
+package Movement;
+
+public class Plateau {
+
+    private PlateauSize size;
+
+    public Plateau(PlateauSize size) {
+        this.size = size;
+    }
+
+    public boolean isValidPosition(Position position) {
+        return position.get
+    }
+
+}
